9897aa2090d86b7b609986024bb576f306796ff8
Brad Swan <bradj.swan@gmail.com>
2018-03-19 00:06:20 +1000

    stock-picker produces correct result

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,12 +1,20 @@
 prices = [17,3,6,9,15,8,6,1,10]
 
-return_ratio = 0
+profit = 0
+bought = 0
+sold = 0
 
-(prices.first prices.size - 1).each_with_index do |buy, index|
-	puts "Buy: #{buy}, Index: #{index}"
-
-	prices.drop(index + 1).each do |sell|
-		puts "Sell: #{sell}"
+(prices.first prices.size - 1).each_with_index do |buy, i|
+	# puts "Buy: #{buy}, Index: #{i}"
+	prices.drop(i + 1).each_with_index do |sell, j|
+		temp_profit = sell - buy
+		if temp_profit > profit
+			profit = temp_profit
+			bought = i
+			sold = j + (i + 1)
+		end
+		# puts "Sell: #{sell}, Profit: #{profit} Index: #{j}"
 	end
 
 end
+puts "Bought: #{bought}, Sold: #{sold}"

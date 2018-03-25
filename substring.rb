154b053e237d65b73eb3db616a68cc815a62f2c6
Brad Swan <bradj.swan@gmail.com>
2018-03-25 12:23:56 +1000

    finalized substring.rb

--- substring.rb
+++ substring.rb
@@ -1,16 +1,13 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-subs_array = Hash.new(0)
-
-text = "Howdy partner, sit down! How's it going?"
-text = text.downcase.split(" ")
-  
-text.each do |word|
-  dictionary.each do |match|
-    if word.include? match
-      subs_array[match] += 1
+def substrings(text, dictionary)
+  text = gets.chomp
+  dictionary = gets.chomp.split(" ")
+  subs_array = Hash.new(0)
+  text.each do |word|
+    dictionary.each do |match|
+      if word.include? match
+        subs_array[match] += 1
+      end
     end
   end
+  puts subs_array
 end
-
-puts subs_array

11e0e66f732e429b201079b84c254aa98a867f8d
Brad Swan <bradj.swan@gmail.com>
2018-03-25 12:05:06 +1000

    substring.rb added, stock-picker.rb optimizations

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,20 +1,13 @@
-prices = [17,3,6,9,15,8,6,1,10]
-
-profit = 0
-bought = 0
-sold = 0
-
-(prices.first prices.size - 1).each_with_index do |buy, i|
-	# puts "Buy: #{buy}, Index: #{i}"
-	prices.drop(i + 1).each_with_index do |sell, j|
-		temp_profit = sell - buy
-		if temp_profit > profit
-			profit = temp_profit
-			bought = i
-			sold = j + (i + 1)
+def stock_picker (prices)
+	profit = 0
+	(prices.first prices.size - 1).each_with_index do |buy, i|
+		prices.drop(i + 1).each_with_index do |sell, j|
+			if (sell - buy) > profit
+				profit = sell - buy
+				@bought = i
+				@sold = j + i + 1
+			end
 		end
-		# puts "Sell: #{sell}, Profit: #{profit} Index: #{j}"
 	end
-
+	return [bought, sold]
 end
-puts "Bought: #{bought}, Sold: #{sold}"

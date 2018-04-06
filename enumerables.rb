6145a352a2dbd9cc5710d15e9f1395f7e1a94200
Brad Swan <bradj.swan@gmail.com>
2018-04-06 18:08:42 +1000

    added new enumerable methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,4 +1,25 @@
 module Enumerable
+  
   def my_each
-    self.size.times { |i| yeild(self[i]) }
+    self.size.times { |i| yield(self[i]) }
   end
+  
+  def my_each_with_index
+    i = 0
+    while i < self.size do
+      yield(self[i], i)
+      i += 1
+    end      
+  end
+  
+  def my_select
+    selected = []
+    self.my_each do |i|
+      if yield(self[i - 1]) == true
+        selected << self[i - 1] 
+      end
+    end
+    return selected
+  end
+  
+end

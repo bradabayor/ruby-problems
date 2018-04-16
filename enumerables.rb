85f83123c29a56c2ef402d1266555dc53b02bcb5
Brad Swan <bradj.swan@gmail.com>
2018-04-16 18:58:16 +1000

    added #my_inject

--- enumerables.rb
+++ enumerables.rb
@@ -17,11 +17,69 @@ module Enumerable
   def my_select
     selected = []
     self.my_each do |i|
-      if yield(self[i - 1]) == true
-        selected << self[i - 1] 
+      if yield(i) == true
+        selected << i
       end
     end
     selected
   end
-
+  
+  def my_all?
+    self.my_each do |i|
+      yield(i) == true ? next : (return false)
+    end
+    true
+  end
+  
+  def my_any?
+    self.my_each do |i|
+      yield(i) == true ? (return true) : next
+    end
+    false
+  end
+  
+  def my_none?
+    self.my_each do |i|
+      yield(i) == true ? (return false) : next
+    end
+    true
+  end
+  
+  def my_count(*args)
+    count = 0
+    if block_given?
+      self.my_each do |i|
+        yield(i) == true ? (count += i) : next
+      end
+    elsif args.empty?
+      self.my_each do |i|
+        count += i
+      end
+    else
+      arg = args[0]
+      self.my_each do |i|
+        i == arg ? (count += i) : next
+      end
+    end
+    count
+  end
+  
+  def my_map
+    arr = []
+    self.my_each do |i|
+      arr << yield(i)
+    end
+    arr
+  end
+  
+  def my_inject(inital = 0)
+    value = inital
+    self.my_each do |i|
+      value += yield(value, i)
+    end
+    value
+  end
+  
 end
+
+[1,2,3].my_inject { |sum, n| sum + n }
